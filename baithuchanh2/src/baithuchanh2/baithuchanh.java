9ddc193d9b8379dd243d25e2a7e43008367138e5
Đinh Huỳnh Vũ <153527266+dinhvune12@users.noreply.github.com>
2025-05-16 23:36:42 +0700

    duacodelan2

--- baithuchanh2/src/baithuchanh2/baithuchanh.java
+++ baithuchanh2/src/baithuchanh2/baithuchanh.java
@@ -1,6 +1,7 @@
 package baithuchanh2;
 
 import java.awt.EventQueue;
+import java.awt.Window;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -12,8 +13,6 @@ import java.awt.event.ActionEvent;
 public class baithuchanh {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -47,58 +46,88 @@ public class baithuchanh {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(169, 63, 96, 19);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(169, 123, 96, 19);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(72, 66, 45, 13);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(72, 126, 45, 13);
-		frame.getContentPane().add(lblNewLabel_1);
-		
 		JButton btnNewButton = new JButton("chucnang1");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 chucnang1 x = new chucnang1();
+			        x.setVisible(true); 
+			}
+		});
 		btnNewButton.setBounds(59, 271, 85, 21);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("chucnang2");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 chucnang2 x = new chucnang2();
+			        x.setVisible(true); 
+			}
+		});
 		btnNewButton_1.setBounds(261, 271, 85, 21);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("chucnang3");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 chucnang3 x = new chucnang3();
+			        x.setVisible(true); 
+			}
+		});
 		btnNewButton_2.setBounds(464, 271, 85, 21);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("chucnang4");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chucnang4 x = new chucnang4();
+		        x.setVisible(true); 
+			}
+		});
 		btnNewButton_3.setBounds(660, 271, 85, 21);
 		frame.getContentPane().add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("chucnang5");
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				 chucnang5 x = new chucnang5();
+			        x.setVisible(true); 
 			}
 		});
 		btnNewButton_4.setBounds(72, 352, 85, 21);
 		frame.getContentPane().add(btnNewButton_4);
 		
 		JButton btnNewButton_5 = new JButton("chucnang6");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chucnang6 x = new chucnang6();
+		        x.setVisible(true); 
+			}
+		});
 		btnNewButton_5.setBounds(261, 352, 85, 21);
 		frame.getContentPane().add(btnNewButton_5);
 		
 		JButton btnNewButton_6 = new JButton("chucnang7");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chucnang7 x = new chucnang7();
+		        x.setVisible(true); 
+			}
+		});
 		btnNewButton_6.setBounds(464, 352, 85, 21);
 		frame.getContentPane().add(btnNewButton_6);
 		
 		JButton btnNewButton_7 = new JButton("chucnang8");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chucnang8 x = new chucnang8();
+		        x.setVisible(true); 
+			}
+		});
 		btnNewButton_7.setBounds(660, 352, 85, 21);
 		frame.getContentPane().add(btnNewButton_7);
+		
+		JLabel lblNewLabel = new JLabel("tong hop chuc nang");
+		lblNewLabel.setBounds(140, 79, 295, 13);
+		frame.getContentPane().add(lblNewLabel);
 	}
 }
